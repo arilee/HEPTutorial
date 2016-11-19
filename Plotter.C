45050131e1092849c5245b53ef2da71c762bb7c3
TAE JEONG KIM <monttj@gmail.com>
2016-11-20 02:47:11 +0900

    a little cosmetic update

--- Plotter.C
+++ Plotter.C
@@ -19,9 +19,26 @@ Plotter::~Plotter() {
 void Plotter::Plot(std::string filename) {
    
 	gROOT->Reset();
-	//gROOT->SetStyle("Plain");
+	gROOT->SetStyle("Plain");
    
 	TStyle *MyStyle = new TStyle("MyStyle","My Root Styles");
+
+        MyStyle->SetCanvasDefH(150);
+        MyStyle->SetCanvasDefW(150);
+
+        MyStyle->SetAxisColor(1, "XYZ");
+        MyStyle->SetStripDecimals(1);
+        MyStyle->SetTickLength(0.03, "XYZ");
+        MyStyle->SetNdivisions(510, "XYZ");
+        MyStyle->SetPadTickX(1);
+        MyStyle->SetPadTickY(1);  
+
+        MyStyle->SetTitleColor(1, "XYZ");
+        MyStyle->SetTitleFont(42, "XYZ");
+
+        MyStyle->SetLabelColor(1, "XYZ");
+        MyStyle->SetLabelFont(42, "XYZ");
+
 	MyStyle->SetStatColor(0);
 	MyStyle->SetCanvasColor(0);
 	MyStyle->SetPadColor(0);
@@ -31,39 +48,34 @@ void Plotter::Plot(std::string filename) {
 	MyStyle->SetOptStat(0);
 	MyStyle->SetStatBorderSize(2);
 	MyStyle->SetOptTitle(0);
-	MyStyle->SetPadTickX(1);
-	MyStyle->SetPadTickY(1);
 	MyStyle->SetPadBorderSize(2);
 	MyStyle->SetPalette(51, 0);
 	MyStyle->SetPadBottomMargin(0.15);
-	MyStyle->SetPadTopMargin(0.05);
+	MyStyle->SetPadTopMargin(0.10);
 	MyStyle->SetPadLeftMargin(0.15);
-	MyStyle->SetPadRightMargin(0.25);
-	MyStyle->SetTitleColor(1);
+	MyStyle->SetPadRightMargin(0.10);
 	MyStyle->SetTitleFillColor(0);
 	MyStyle->SetTitleFontSize(0.05);
 	MyStyle->SetTitleBorderSize(0);
 	MyStyle->SetLineWidth(1);
 	MyStyle->SetHistLineWidth(3);
 	MyStyle->SetLegendBorderSize(0);
-	MyStyle->SetNdivisions(502, "x");
-	MyStyle->SetMarkerSize(0.8);
-	MyStyle->SetTickLength(0.03);
+	MyStyle->SetMarkerSize(1);
 	MyStyle->SetTitleOffset(1.5, "x");
 	MyStyle->SetTitleOffset(1.5, "y");
 	MyStyle->SetTitleOffset(1.0, "z");
-	MyStyle->SetLabelSize(0.05, "x");
-	MyStyle->SetLabelSize(0.05, "y");
-	MyStyle->SetLabelSize(0.05, "z");
-	MyStyle->SetLabelOffset(0.03, "x");
-	MyStyle->SetLabelOffset(0.03, "y");
-	MyStyle->SetLabelOffset(0.03, "z");
+	MyStyle->SetLabelSize(0.06, "x");
+	MyStyle->SetLabelSize(0.06, "y");
+	MyStyle->SetLabelSize(0.06, "z");
+	MyStyle->SetLabelOffset(0.15, "x");
+	MyStyle->SetLabelOffset(0.15, "y");
+	MyStyle->SetLabelOffset(0.15, "z");
 	MyStyle->SetTitleSize(0.05, "x");
 	MyStyle->SetTitleSize(0.05, "y");
 	MyStyle->SetTitleSize(0.05, "z");
 	gROOT->SetStyle("MyStyle");
    
-	//gROOT->ForceStyle();
+	gROOT->ForceStyle();
    
 	bool DrawLog = true;
    
@@ -74,7 +86,8 @@ void Plotter::Plot(std::string filename) {
 		int Nset = data.size() + bg.size() + signal.size();
 		if (Nset > 20)
 			Nset = 20.;
-		l = new TLegend(0.76, 0.95 - 0.8 * Nset / 20., 1.0, 0.95);
+		//l = new TLegend(0.76, 0.95 - 0.8 * Nset / 20., 1.0, 0.95);
+		l = new TLegend(0.70, 0.87 - 0.8 * Nset / 20., 0.87, 0.87);
 		l->SetFillStyle(1001);
 		l->SetFillColor(kWhite);
 		l->SetLineColor(kWhite);
@@ -125,20 +138,21 @@ void Plotter::Plot(std::string filename) {
 			}
 		}
 		//TCanvas *c = new TCanvas("c", "c", 800, 600);
-                TCanvas *c = new TCanvas(Form("c_%i",i), "c", 800,600);
+                TCanvas *c = new TCanvas(Form("c_%i",i), "c", 1 );
 		c->SetLogy(DrawLog);
 		std::string plotname;
 		if (data.size() > 0) {
 			plotname = std::string(data.at(0).at(i)->GetName());
 			data.at(0).at(i)->SetMaximum(5 * data.at(0).at(i)->GetMaximum());
-			data.at(0).at(i)->GetXaxis()->SetTitleOffset(1.3);
-			data.at(0).at(i)->GetYaxis()->SetTitleOffset(1.3);
+			data.at(0).at(i)->GetXaxis()->SetTitleOffset(1.5);
+			data.at(0).at(i)->GetYaxis()->SetTitleOffset(1.5);
 			data.at(0).at(i)->GetYaxis()->SetTitle("Events");
 			data.at(0).at(i)->GetXaxis()->SetNdivisions(505);
 			data.at(0).at(i)->Draw("");
 			l->AddEntry(data.at(0).at(i), data_names.at(0).c_str(), "p");
 			if (bg.size() > 0) hs->Draw("histsame");
 			data.at(0).at(i)->SetMarkerStyle(20);
+			data.at(0).at(i)->SetMarkerSize(0.3);
 			data.at(0).at(i)->Draw("psame");
 			l->Draw("same");
 		}
@@ -146,14 +160,16 @@ void Plotter::Plot(std::string filename) {
 			plotname = std::string(bg.at(0).at(i)->GetName());
                         hs->Draw("hist");
                         hs->SetMaximum(5 * bg.at(0).at(i)->GetMaximum());
-                        hs->GetXaxis()->SetTitleOffset(1.3);
-                        hs->GetYaxis()->SetTitleOffset(1.3);
+                        hs->GetXaxis()->SetTitleOffset(1.5);
+                        hs->GetYaxis()->SetTitleOffset(1.5);
                         hs->GetYaxis()->SetTitle("Events");
                         hs->GetXaxis()->SetNdivisions(505);
                         hs->GetXaxis()->SetTitle(bg.at(0).at(i)->GetXaxis()->GetTitle());
 			l->Draw("same");
 		}
-//      c->Print((filename+std::string("_")+plotname+std::string(".pdf")).c_str());
+              
+                c->Print((std::string("plot_")+plotname+std::string(".pdf")).c_str());
+
 		if (i == 0 && N_histos > 1){
 			c->Print((filename+std::string("(")).c_str());
 		}else if (i > 0 && i == N_histos - 1){
